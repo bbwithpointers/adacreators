75c151d973382514b66c2ef88c37c301e7d851ed
Bruno Barros <contato4code@gmail.com>
2022-10-16 17:48:57 -0300

    Init project

---
+++ src/main/java/com/adacreators/AdacreatorsApplication.java
@@ -0,0 +1,13 @@
+package com.adacreators;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdacreatorsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdacreatorsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/adacreators/AdacreatorsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adacreators;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdacreatorsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
